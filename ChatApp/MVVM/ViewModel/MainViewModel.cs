ece27adec014575834c982e6d0247c1f31b546b6
Mille Elfver <mille.elfver98@gmail.com>
2024-08-16 09:08:18 +0200

    Added to ctor and now can click enter to "send" message

--- ChatApp/MVVM/ViewModel/MainViewModel.cs
+++ ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -8,9 +8,10 @@ public class MainViewModel : ObservableObject
 {
     public ObservableCollection<MessageModel> Messages { get; set; }
     public ObservableCollection<ContactModel> Contacts { get; set; }
-    
+
     // Commands
-    
+    public RelayCommand SendCommand { get; set; }
+
     public ContactModel SelectedContact { get; set; }
 
     private string _message;
@@ -23,18 +24,30 @@ public class MainViewModel : ObservableObject
             _message = value;
             OnPropertyChanged();
         }
-    }   
-    
+    }
+
     public MainViewModel()
     {
         Messages = new ObservableCollection<MessageModel>();
         Contacts = new ObservableCollection<ContactModel>();
-        
+
+        SendCommand = new RelayCommand(o =>
+        {
+            Messages.Add(new MessageModel
+            {
+                Message = Message,
+                FirstMessage = false
+            });
+            Message = "";
+        });
+
+
         Messages.Add(new MessageModel
         {
-            Username = "John Doe", 
+            Username = "John Doe",
             UsernameColor = "#409aff",
-            ImageSource = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnzc4pyjJSRrR9s3boT_97OC7t2ItfdFGd2A&s",
+            ImageSource =
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnzc4pyjJSRrR9s3boT_97OC7t2ItfdFGd2A&s",
             Message = "Hello, World!",
             Time = DateTime.Today,
             IsNativeOrigin = false,
@@ -45,34 +58,37 @@ public class MainViewModel : ObservableObject
         {
             Messages.Add(new MessageModel
             {
-                Username = "John Doe", 
+                Username = "John Doe",
                 UsernameColor = "#409aff",
-                ImageSource = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnzc4pyjJSRrR9s3boT_97OC7t2ItfdFGd2A&s",
+                ImageSource =
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnzc4pyjJSRrR9s3boT_97OC7t2ItfdFGd2A&s",
                 Message = "Hello, World!",
                 Time = DateTime.Today,
                 IsNativeOrigin = false,
                 FirstMessage = false
             });
         }
-        
+
         for (int i = 0; i < 4; i++)
         {
             Messages.Add(new MessageModel
             {
-                Username = "Bunny Doe", 
+                Username = "Bunny Doe",
                 UsernameColor = "#409aff",
-                ImageSource = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnzc4pyjJSRrR9s3boT_97OC7t2ItfdFGd2A&s",
+                ImageSource =
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnzc4pyjJSRrR9s3boT_97OC7t2ItfdFGd2A&s",
                 Message = "Hello, World!",
                 Time = DateTime.Today,
                 IsNativeOrigin = true
             });
         }
-        
+
         Messages.Add(new MessageModel
         {
-            Username = "Bunny Doe", 
+            Username = "Bunny Doe",
             UsernameColor = "#409aff",
-            ImageSource = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnzc4pyjJSRrR9s3boT_97OC7t2ItfdFGd2A&s",
+            ImageSource =
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnzc4pyjJSRrR9s3boT_97OC7t2ItfdFGd2A&s",
             Message = "Last",
             Time = DateTime.Today,
             IsNativeOrigin = true
@@ -83,7 +99,8 @@ public class MainViewModel : ObservableObject
             Contacts.Add(new ContactModel
             {
                 Username = $"Allison {i}",
-                ImageSource = "https://yt3.googleusercontent.com/vw3uP9icAdob-2IT3AW4GKBLbd_aW0MWO2cYIjXUQgWynZEmqNo8Z6HVfK-7UndPt9KwR0WDlA=s900-c-k-c0x00ffffff-no-rj",
+                ImageSource =
+                    "https://yt3.googleusercontent.com/vw3uP9icAdob-2IT3AW4GKBLbd_aW0MWO2cYIjXUQgWynZEmqNo8Z6HVfK-7UndPt9KwR0WDlA=s900-c-k-c0x00ffffff-no-rj",
                 Messages = Messages
             });
         }
